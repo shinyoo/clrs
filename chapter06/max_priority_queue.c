07a8d085832634fa31116afb0ccc242ea69bdd9d
shinyoo <shinyoo.dzan@gmail.com>
2017-06-07 22:35:12 +0800

    heap_delete

--- chapter06/max_priority_queue.c
+++ chapter06/max_priority_queue.c
@@ -1,5 +1,6 @@
 #include 'heap.c'
 #define MIN -32766
+#define MAX 32767
 int heap_maximum(array a) {
 	return a.array[0];
 }
@@ -33,5 +34,16 @@ void heap_increase_key(array a, int i, int key) {
 void max_heap_insert(array a, int key) {
 	a.hn ++;
 	a.array[a.hn] = MIN;
-	heap_increase_key(a, a.hn - 1, key)
+	heap_increase_key(a, a.hn - 1, key);
+}
+
+
+void heap_delete(array a, int i) {
+	//assume we increase a[i] to infinity(which we do not need to do actually), then "bubble" up to the root!
+	//the following loop takes up O(lgn)
+	while (i != 0) {
+		a.array[i] = a.array[PARENT];
+		i = PARENT(i);
+	}
+	heap_extract_max(a);	//O(lgn)
 }
